1681191ed02caaa8a6840ecec04d2e00cdfed566
William Johansson <radar@radhuset.org>
2020-02-19 23:25:09 +0100

    Initial version

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/radhus/google-cloud-dyndns
+
+go 1.13
+
+require google.golang.org/api v0.17.0
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	dns "google.golang.org/api/dns/v1"
+)
+
+var (
+	address = flag.String("address", "", "Address to set (required or -interface)")
+	ifName  = flag.String("interface", "", "Interface to get first address from (required or -address)")
+	host    = flag.String("host", "", "Host to update (required)")
+	project = flag.String("project", "", "GCP project (required)")
+	zoneID  = flag.String("zone", "", "GCP Zone ID (required)")
+)
+
+func usageExit(err string) {
+	fmt.Println(err)
+	fmt.Println()
+	flag.Usage()
+	os.Exit(1)
+}
+
+func main() {
+	flag.Parse()
+
+	if (*address == "" && *ifName == "") || *host == "" || *project == "" || *zoneID == "" {
+		usageExit("(-address or -interface), -host, -project and -zone are required")
+	}
+	if *address != "" && *ifName != "" {
+		usageExit("-address and -interface are mutually exclusive")
+	}
+
+	logger := log.New(os.Stdout, fmt.Sprintf("[%s] ", *host), 0)
+
+	wantedAddress := *address
+	if *ifName != "" {
+		iface, err := net.InterfaceByName(*ifName)
+		if err != nil {
+			logger.Fatalln("Couldn't get interface:", err)
+		}
+		ifAddrs, err := iface.Addrs()
+		if err != nil {
+			logger.Fatalln("Couldn't get interface addrs:", err)
+		}
+		if len(ifAddrs) == 0 {
+			logger.Fatalln("No interface addresses")
+		}
+		ifaceIP, _, err := net.ParseCIDR(ifAddrs[0].String())
+		if err != nil {
+			logger.Fatalln("Couldn't parse interface address:", ifAddrs[0].String())
+		}
+		wantedAddress = ifaceIP.String()
+	}
+
+	ctx := context.Background()
+
+	dnsService, err := dns.NewService(ctx)
+	if err != nil {
+		logger.Fatalln("Failed to create DNS service:", err)
+	}
+
+	errDone := fmt.Errorf("done")
+	var record *dns.ResourceRecordSet
+	err = dnsService.ResourceRecordSets.List(*project, *zoneID).Pages(
+		ctx,
+		func(page *dns.ResourceRecordSetsListResponse) error {
+			for _, r := range page.Rrsets {
+				if r.Name == *host && r.Type == "A" {
+					record = r
+					return errDone
+				}
+			}
+			return nil
+		},
+	)
+	if err != nil && !errors.Is(err, errDone) {
+		logger.Fatalln("Error listing ResourceRecordSets:", err)
+	}
+	if record == nil {
+		logger.Fatalln("No record found")
+	}
+
+	if len(record.Rrdatas) > 1 {
+		logger.Fatalln("Unexpected amount of record values:", record.Rrdatas)
+	}
+
+	currentAddress := record.Rrdatas[0]
+	if wantedAddress == currentAddress {
+		logger.Println("No change needed:", wantedAddress)
+		return
+	}
+
+	logger.Printf("Updating %s to %s...", *host, wantedAddress)
+	change := &dns.Change{
+		Additions: []*dns.ResourceRecordSet{{
+			Name:    *host,
+			Type:    "A",
+			Ttl:     record.Ttl,
+			Rrdatas: []string{wantedAddress},
+		}},
+		Deletions: []*dns.ResourceRecordSet{record},
+	}
+
+	_, err = dnsService.Changes.Create(*project, *zoneID, change).Context(ctx).Do()
+	if err != nil {
+		logger.Fatalln("Failed to update record:", err)
+	}
+	logger.Println("Updated.")
+}
